775199822807e5d14c491f13445f198b247286e3
yggdrasilyuan <1942767016@qq.com>
2022-10-10 10:09:18 -0500

    sync

--- financial_computing/assignment4_1.cpp
+++ financial_computing/assignment4_1.cpp
@@ -3,37 +3,37 @@
 #include <algorithm>
 using namespace std;
 
-int a;
-float** matrix;
+int NUM;//size of array
+double** matrix;
 
-float cal_value(int r, int b)
+double cal_value(int r, int b)
 {
     if (r == 0)
-        return (float)b;
+        return (double)b;
     else if (0 == b)
-        return 0;
+        return 0.0;
     else if (matrix[r][b] != 0)
         return matrix[r][b];
     else
     {
-        float p = ((float)r/(r+b)) * cal_value(r-1, b)+((float)b / (r+b)) * cal_value(r, b-1);
-        matrix[r][b] = max((p), (float)(b-r));
+        double p = ((double)r/(r+b)) * cal_value(r-1, b)+((double)b / (r+b)) * cal_value(r, b-1);
+        matrix[r][b] = max((p), (double)(b-r));
         return matrix[r][b];
     }
 }
 
-void go(int n)
+void play(int n)
 {
-    a = n/2+1;
-    matrix = new float*[a];
-    for (int i = 0; i<a; i++)
-        matrix[i] = new float[a];
+    NUM = n/2+2;// get more space to put all possibilities
+    matrix = new double*[NUM];
+    for (int i = 0; i<NUM; i++)
+        matrix[i] = new double[NUM];
 
-    for (int i = 0; i < a; i++)
+    for (int i = 0; i < NUM; i++)
     {
-        for (int j = 0; j < a; j++)
+        for (int j = 0; j < NUM; j++)
         {
-            matrix[i][j] = 0;
+            matrix[i][j] = 0;//init
         }
     }
 }
@@ -42,11 +42,11 @@ int main(int argc,char* argv[])
 {
 	int num;
 	cout << "Please input total number of cards!" << endl;
-	// num = atoi(argv[1]);
-    cin>>num;
+    // cin>>num;
+    num=atoi(argv[1]);
 	cout << "Total Number of Cards = " << num << endl;
-	go(num);
-	float value = cal_value(num/2, num/2);
+	play(num);
+	double value = cal_value(num/2, num/2);
 	cout << "Value of the game = " << value << endl;
     system("pause");
 	return 0;
