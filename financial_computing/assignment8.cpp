ddfe4626f72dfedc6fdf6f9092bdf0080f4cd8bb
yggdrasilyuan <1942767016@qq.com>
2022-11-10 21:27:59 -0600

    sync

--- financial_computing/assignment8.cpp
+++ financial_computing/assignment8.cpp
@@ -4,18 +4,15 @@
 #include <fstream>
 #include <cstdlib>
 #include <random>
-#include <chrono>
 #include <stdio.h>
-
 #include <vector>
 using namespace std;
-
-unsigned seed = (unsigned) std::chrono::system_clock::now().time_since_epoch().count();
-default_random_engine generator(seed);
-const double PI = 3.141592653589793238463;    //value of pi
-
 #define CAUCHY_DENSITY(x) 1./(PI * (1 + x * x))
 #define GAUSSIAN_DENSITY(x) 0.398942280375387*exp(-x*x/2.0)
+
+unsigned seed = (unsigned)time(NULL);
+default_random_engine generator(seed);
+const double PI = 3.141592653589793238463;    //value of pi
 
 double get_uniform()
 {
@@ -28,73 +25,71 @@ double get_cauchy()
   return tan(PI * (get_uniform() - 0.5));
 }
 
-vector<int> gen_counts(int no_of_trials)
+vector<int> generate(int no_of_trials)
 {
   double y;
   vector<int> count(101, 0);
-  for (int i = 0; i < no_of_trials; i++)
+  for (int flag = 0; flag < no_of_trials; flag++)
   {
     y = get_cauchy();
-    for (int j = 0; j <= 100; j++)
-      if ( (y >= ((float) (j-51)/10)) && (y < ((float) (j-50)/10)) )
-        count[j]++;
+    for (int flag = 0; flag <= 100; flag++)
+      if ( (y >= ((float) (flag-51)/10)) && (y < ((float) (flag-50)/10)) )
+        count[flag]++;
   }
   return count;
 }
 
-void writeout(vector<double> &n ,vector<double> &theory_cauchy, vector<double> &exp_cauchy, vector<double> &theory_gaussian)
-{
-  ofstream logfile("res.csv");
-  if (logfile.is_open())
-  {
-    logfile << "n,Theory_Cauchy,Experiment_Cauchy,Thoery_Gaussian";
-    logfile << "\n";
-    for (int i = 0; i < n.size(); i++)
-    {
-      logfile << n[i] << "," << theory_cauchy[i] << "," << exp_cauchy[i] << "," << theory_gaussian[i] << "\n";
-    }
-  }
-  logfile.close();
-}
 
 
 
 int main(int argc, const char* argv[])
-{
-  vector<double> x(101, 0.);
-  vector<double> cdf_cauchy(100, 0.);
-  vector<double> exp_cdf(100, 0.);
-  vector<double> exp_pdf(101, 0.);
+{  
   int no_of_trials;
-
+  int num=100;
   sscanf(argv[1], "%d", &no_of_trials);
-  vector<int> count = gen_counts(no_of_trials);
+  vector<int> count = generate(no_of_trials);
+  vector<double> theory_cauchy(num+1, 0.);
+  vector<double> theory_gaussian(num+1, 0.);
+  vector<double> n(num+1, 0.);
+  vector<double> cdf_cauchy(num, 0.);
+  vector<double> exp_cdf(num, 0.);
+  vector<double> experiment_cauchy(num+1, 0.);
+  
 
-  vector<double> pdf_cauchy(101, 0.);
-  vector<double> pdf_normal(101, 0.);
-  for (int i = 0; i <= 100; i++)
+  for (int flag = 0; flag <= num; flag++)
   {
-    x[i] = (i - 50.) / 10.;
-    pdf_cauchy[i] = (0.1 * CAUCHY_DENSITY((float) x[i]));
-    pdf_normal[i] = 0.1*GAUSSIAN_DENSITY((float) x[i]);
+    n[flag] = (flag - 50.) / 10.;
+    theory_cauchy[flag] = (0.1 * CAUCHY_DENSITY((float) n[flag]));
+    theory_gaussian[flag] = 0.1*GAUSSIAN_DENSITY((float) n[flag]);
   }
 
-	for (int j = 1; j < 100; j++) {
-    cdf_cauchy[j] = cdf_cauchy[j - 1] + (0.1 * CAUCHY_DENSITY((float) (j - 50) / 10));
+	for (int flag = 1; flag < num; flag++) {
+    cdf_cauchy[flag] = cdf_cauchy[flag - 1] + (0.1 * CAUCHY_DENSITY((float) (flag - 50) / 10));
   }
 
 	int sum = 0;
-	for (int j = 0; j < 100; j++)
+	for (int flag = 0; flag < num; flag++)
   {
-		sum += count[j];
-    exp_cdf[j] = sum / no_of_trials;
+		sum += count[flag];
+    exp_cdf[flag] = sum / no_of_trials;
 	}
-  for (int i = 0; i <=100; i++)
+  for (int flag = 0; flag <=100; flag++)
   {
-    exp_pdf[i] = ((float) count[i]) / no_of_trials;
+    experiment_cauchy[flag] = ((float) count[flag]) / no_of_trials;
   }
 
-  writeout(x, pdf_cauchy, exp_pdf, pdf_normal);
+  ofstream logfile("res.csv");
+  if (logfile.is_open())
+  {
+    logfile << "n,Theory_Cauchy,Experiment_Cauchy,Theory_Gaussian";
+    logfile << endl;
+    for (int flag = 0; flag < n.size(); flag++)
+    {
+      logfile << n[flag] << "," << theory_cauchy[flag] << "," << experiment_cauchy[flag] << "," << theory_gaussian[flag] << endl;
+    }
+  }
+  logfile.close();
+
   system("pause");
   return 0;
 }
